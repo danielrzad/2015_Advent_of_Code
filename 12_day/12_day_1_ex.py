4537d802b52249326a3f32be41fd3edb8e27b70a
Daniel Rząd <daniel96rzad@gmail.com>
2019-07-15 10:51:03 +0200

    Uploading ex. 12
    Just an another exercise.

--- 12_day/12_day_1_ex.py
+++ 12_day/12_day_1_ex.py
@@ -1,24 +1,48 @@
-with open("10_day_input.txt") as data_doc:
-	data = data_doc.read()
+import json
+from pprint import pprint
 
-result = ""
-duplicate_num = 1
-for i in range(40):
-	for i in range(len(data)):
-		try:
-			if data[i] == data[i+1]:
-				duplicate_num += 1
-			elif data[i] == data[i-1] and data[i] != data[i+1]:
-				result += str(duplicate_num)
-				result += data[i]
-				duplicate_num = 1
-			elif data[i] != data[i-1] and data[i] != data[i+1]:
-				result += str(duplicate_num)
-				result += data[i]
-		except IndexError:
-				result += str(duplicate_num)
-				result += data[i]
-	data = result
-	result = ""
+with open("12_day_input.JSON", "r") as data_doc:
+	data = json.load(data_doc)
 
-print(len(data))
+
+#print(type(data))
+#pprint(data)
+
+#for key, value in data.items():
+#	print(type(key), type(value))
+
+
+num_sum = 0
+
+def get_dic_val(dic):
+	global num_sum
+	for val in dic.values():
+		if type(val) == int:
+			num_sum += val
+		if type(val) == list:
+			get_lst_val(val)
+		if type(val) == dict:
+			get_dic_val(val)
+
+
+def get_lst_val(lst):
+	global num_sum
+	for val in lst:
+		if type(val) == int:
+			num_sum += val
+		if type(val) == list:
+			get_lst_val(val)
+		if type(val) == dict:
+			get_dic_val(val)
+
+
+
+for val in data.values():
+	if type(val) == str:
+		continue
+	if type(val) == list:
+		get_lst_val(val)
+	else:
+		get_dic_val(val)
+
+print(num_sum)
