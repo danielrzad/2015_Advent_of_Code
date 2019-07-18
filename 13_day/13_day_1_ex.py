1f34935fcb11b392e5480ee4f9288af467e560fa
Daniel Rząd <daniel96rzad@gmail.com>
2019-07-18 16:00:19 +0200

    ex 13 in progress

---
+++ 13_day/13_day_1_ex.py
@@ -0,0 +1,58 @@
+with open("13_day_input.txt") as data_doc:
+	data = [s.replace(".", "") .split()for s in data_doc.read().splitlines()]
+
+relations = []
+persons = set()
+
+for line in data:
+	persons.add(line[0])
+	persons.add(line[-1])
+	persons_lst = []
+	if line[2] == "gain":
+		persons_lst.append(int(line[3]))
+	elif line[2] == "lose":
+		persons_lst.append(-int(line[3]))
+	persons_lst.append(line[0])
+	persons_lst.append(line[10])
+	relations.append(persons_lst)
+
+connections = relations[:]
+
+
+def find_next(lst):
+	relationships = []
+	for relation in relations:
+		if lst[-1] == relation[1] and relation[2] not in lst:
+			temp_lst = lst[:]
+			temp_lst[0] += relation[0]
+			temp_lst.append(relation[2])
+			if temp_lst not in relationships:
+				relationships.append(temp_lst)
+	return relationships
+
+final_lst = []
+
+while all([len(i) == (len(persons) + 1) for i in connections]) != True:
+	for connection in connections:
+		if len(connection) < 9:
+			connections.extend(find_next(connection))
+			connections.remove(connection)
+		if len(connection) == 9:
+			for rel in relations:
+				if connection[1] == rel[2] and connection[-1] == rel[1]:
+					connection[0] += rel[0]
+					final_lst.append(connection)
+					connections.remove(connection)
+					break
+
+highest_happines = final_lst[0][0]
+print(final_lst)
+for lst in final_lst:
+	if lst[0] > highest_happines:
+		highest_happines = lst[0]
+
+
+
+
+for count, item in reversed(enumerate(final_lst)):
+	print("chuj")
--- test 1.py
+++ test 1.py
@@ -1,13 +1,22 @@
-import re
+relations = [[368, 'Eric', 'Carol', 'Frank', 'George', 'David', 'Alice', 'Bob', 'Mallory'], 
+			[143, 'Eric', 'Frank', 'Bob', 'Alice', 'George', 'Mallory', 'Carol', 'David'],
+			 [50, 'Eric', 'Frank', 'Bob', 'Mallory', 'Alice', 'David', 'Carol', 'George'], 
+			 [18, 'Eric', 'Frank', 'Mallory', 'Bob', 'David', 'Alice', 'Carol', 'George'],]
 
-def next_password(password):
-    while True:
-        password = re.sub(r'([a-y])(z*)$', lambda x: chr(ord(x.group(1))+1) + len(x.group(2))*"a", password)
-        if ("i" in password or "o" in password or "l" in password) or \
-           (len(re.findall(r'([a-z])\1', password)) < 2) or \
-           (len([1 for x, y, z in zip(password, password[1:], password[2:])
-                   if ord(z)-ord(y) == 1 and ord(y)-ord(x) == 1]) == 0): continue
+connecitons = [[368, 'Eric', 'Carol', 'Frank', 'George', 'David', 'Alice', 'Bob', 'Mallory'], 
+			[143, 'Eric', 'Frank', 'Bob', 'Alice', 'George', 'Mallory', 'Carol', 'David'],
+			 [50, 'Eric', 'Frank', 'Bob', 'Mallory', 'Alice', 'David', 'Carol', 'George'], 
+			 [18, 'Eric', 'Frank', 'Mallory', 'Bob', 'David', 'Alice', 'Carol', 'George'],]
 
-        return password
+persons = {'George', 'David', 'Mallory', 'Alice', 'Frank', 'Eric', 'Bob', 'Carol'}
 
-print(next_password("cqjxjnds"))
+print(all([len(i) == 9 for i in relations]))
+
+relations.extend(connecitons)
+
+print(relations)
+
+"""
+while all([len(i) for i in relations]) != 9:
+	print("true")
+"""
